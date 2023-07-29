790a70eefe46834d480a570b515f98c29401006a
sushantapatra <development.experiment@gmail.com>
2023-07-29 20:02:20 +0530

    Events, Functions,State, and Inner Componet calling on Next.Js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,53 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
+
 export default function Home() {
+	const [name, setName] = useState("");
+	function Greeting() {
+		alert("Welcome to Next");
+	}
+	const Hello = () => {
+		alert("Hello, How are you.");
+	};
+	const Name = (username) => {
+		setName(username);
+		alert("My name is :" + username);
+	};
+	//component ke  andar component
+	const InnerComponent = () => {
+		return (
+			<div>
+				<h1>Hi I am Inner Component</h1>
+			</div>
+		);
+	};
 	return (
 		<main className={styles.main}>
-			<h1>Home Page</h1>
-			{/** Data passing in component */}
-			<User name="Sushnat" />
-			<User name="Harekrushna" />
-			<Name name="Rajbeer" />
-			<Name name="Harisankar" />
+			<h1>Events, Functions and State {name}</h1>
+			<button
+				className="bg-blue-700 text-white rounded-md p-2 m-2"
+				onClick={() => Greeting()}
+			>
+				Normal Function
+			</button>
+			<button
+				className="bg-blue-700 text-white rounded-md p-2 m-2"
+				onClick={Hello}
+			>
+				Arrow Function
+			</button>
+			<button
+				className="bg-blue-700 text-white rounded-md p-2 m-2"
+				onClick={() => Name("Sushanta")}
+			>
+				Arrow Function with passing argument
+			</button>
+
+			{/** Inner Componet calling as normal way*/}
+			<InnerComponent />
+			{/** Inner Componet calling as a function*/}
+			{InnerComponent()}
 		</main>
 	);
 }
@@ -15,23 +55,3 @@ export default function Home() {
 /**
  *Data passing in component
  */
-//without object disturaction
-const Name = (props) => {
-	return (
-		<div>
-			<h1>Name Component</h1>
-			<h1>My Name is {props.name}</h1>
-		</div>
-	);
-};
-
-// object disstructing
-const User = ({ name }) => {
-	console.log(name);
-	return (
-		<div>
-			<h1>User Component</h1>
-			<h1>User name is {name}</h1>
-		</div>
-	);
-};
