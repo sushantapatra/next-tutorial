1f72b56e00bbe8c29dfd7709330646a788693b58
sushantapatra <development.experiment@gmail.com>
2023-08-16 22:10:19 +0530

    Add data in mongodb

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -12,3 +12,16 @@ export async function GET() {
 	}
 	return NextResponse.json({ data: data }, { status: 200 });
 }
+
+export async function POST(request, response) {
+	//getting json data
+	const payload = await request.json();
+	console.log(payload);
+	try {
+		connectDB();
+		const newProduct = new productModel(payload);
+		const result = await newProduct.save();
+	} catch (error) {
+		//console.log(error);
+	}
+}
